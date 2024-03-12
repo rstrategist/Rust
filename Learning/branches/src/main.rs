8e4ac988221dd4600279d5342a8e7345533fea2b
rstrategist <rstrategist@users.noreply.github.com>
2024-03-12 15:52:27 +0100

    Adding Control Flow: Loops and Conditionals

--- Learning/branches/src/main.rs
+++ Learning/branches/src/main.rs
@@ -1,9 +1,21 @@
 fn main() {
-    let number = 3;
+    let number = 6;
 
-    if number < 5 {
-        println!("condition was true");
+    if number % 4 == 0 {
+        println!("The number {number} is divisible by 4");
+    } else if number % 3 == 0 {
+        println!("The number {number} is divisible by 3");
+    } else if number % 2 == 0 {
+        println!("The number {number} is divisible by 2");
     } else {
-        println!("condition was false");
+        println!("The number {number} is not divisible by 4, 3, or 2");
     }
+    assignment_with_if()
+}
+
+// Both variables in an if condition must be of the same type
+fn assignment_with_if() {
+    let condition = true;
+    let number = if condition {5} else {6};
+    println!("The value of number is: {number}");
 }
