e9733b32518415930a890c3dfa69f0851919961c
rstrategist <rstrategist@users.noreply.github.com>
2024-09-08 23:09:11 +0200

    Data structures
    Strcuts
    Enums

---
+++ Learning/course_rust_fundamentals/examples/17-data-structures/src/main.rs
@@ -0,0 +1,29 @@
+mod data;
+use data::{Data, Direction};
+
+fn main() {
+    // Create an instance of out Data structure and populate it with data
+    // for later use.
+    let new_data = Data {
+        name: "Rashid".to_string(),
+        age: 7,
+    };
+
+    // Here we reference the name and age fields in our instance of the Data with
+    // the dot notation
+    println!("Hello, {}", new_data.name);
+    println!("Your favorite number is {}", new_data.age);
+
+    // initialize and access enum variants
+    let north = Direction::North;
+    let east = Direction::East;
+    let south = Direction::South;
+    let west = Direction::West;
+
+    // print enum values making use of the debug marker ":?" as the data type does
+    // not implement the display trait (traits are covered later)
+    println!("{:?}", north);
+    println!("{:?}", east);
+    println!("{:?}", south);
+    println!("{:?}", west);
+}
